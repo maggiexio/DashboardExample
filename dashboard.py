7ae556498df972a1388e3cab79eb49d850271bea
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-02-23 22:07:47 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ import time
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import tk
+#import tkinter
 import tkinter.messagebox
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
   
